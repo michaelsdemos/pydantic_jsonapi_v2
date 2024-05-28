0adde80c9e3a0883d7b586eb01e3c771237c780c
Sören Michaels <michaels@demos-deutschland.de>
2024-05-28 16:25:50 +0200

    feat: add default value

--- pydantic_jsonapi/request.py
+++ pydantic_jsonapi/request.py
@@ -6,32 +6,37 @@ from pydantic import BaseModel, UUID4
 from pydantic_jsonapi.relationships import RequestRelationshipsType
 
 
-TypeT = TypeVar('TypeT')
-AttributesT = TypeVar('AttributesT')
+TypeT = TypeVar("TypeT")
+AttributesT = TypeVar("AttributesT")
+
+
 class RequestDataModel(BaseModel, Generic[TypeT, AttributesT]):
-    """
-    """
+    """ """
+
     type: TypeT
     attributes: AttributesT
-    id: Optional[str]
-    relationships: Optional[RequestRelationshipsType]
+    id: Optional[str] = None
+    relationships: Optional[RequestRelationshipsType] = None
 
 
-DataT = TypeVar('DataT', bound=RequestDataModel)
+DataT = TypeVar("DataT", bound=RequestDataModel)
+
+
 class RequestModel(BaseModel, Generic[DataT]):
-    """
-    """
+    """ """
+
     data: DataT
 
     def attributes(self):
         return self.data.attributes
+
 
 def JsonApiRequest(type_string: str, attributes_model: Any) -> Type[RequestModel]:
     request_data_model = RequestDataModel[
         Literal[type_string],
         attributes_model,
     ]
-    request_data_model.__name__ = f'RequestData[{type_string}]'
+    request_data_model.__name__ = f"RequestData[{type_string}]"
     request_model = RequestModel[request_data_model]
-    request_model.__name__ = f'Request[{type_string}]'
+    request_model.__name__ = f"Request[{type_string}]"
     return request_model
--- pydantic_jsonapi/response.py
+++ pydantic_jsonapi/response.py
@@ -7,34 +7,33 @@ from pydantic_jsonapi.resource_links import ResourceLinks
 from pydantic import BaseModel, ConfigDict
 
 
-TypeT = TypeVar('TypeT', bound=str)
-AttributesT = TypeVar('AttributesT')
+TypeT = TypeVar("TypeT", bound=str)
+AttributesT = TypeVar("AttributesT")
+
+
 class ResponseDataModel(BaseModel, Generic[TypeT, AttributesT]):
-    """
-    """
+    """ """
+
     id: str
     type: TypeT
     attributes: AttributesT = {}
-    relationships: Optional[ResponseRelationshipsType]
-    links: Optional[ResourceLinks]
+    relationships: Optional[ResponseRelationshipsType] = None
+    links: Optional[ResourceLinks] = None
     model_config = ConfigDict(validate_default=True)
 
 
-DataT = TypeVar('DataT', bound=ResponseDataModel)
-class ResponseModel(BaseModel, Generic[DataT]):
-    """
-    """
-    data: DataT
-    included: Optional[list]
-    meta: Optional[dict]
-    links: Optional[ResourceLinks]
+DataT = TypeVar("DataT", bound=ResponseDataModel)
 
-    def dict(
-        self,
-        *,
-        serlialize_none: bool = False,
-        **kwargs
-    ):
+
+class ResponseModel(BaseModel, Generic[DataT]):
+    """ """
+
+    data: DataT
+    included: Optional[list] = None
+    meta: Optional[dict] = None
+    links: Optional[ResourceLinks] = None
+
+    def dict(self, *, serlialize_none: bool = False, **kwargs):
         response = super().dict(**kwargs)
         if serlialize_none:
             return response
@@ -49,10 +48,10 @@ class ResponseModel(BaseModel, Generic[DataT]):
         relationships: Optional[dict] = None,
         links: Optional[dict] = None,
     ) -> ResponseDataModel:
-        data_type = get_type_hints(cls)['data']
-        if getattr(data_type, '__origin__', None) is list:
+        data_type = get_type_hints(cls)["data"]
+        if getattr(data_type, "__origin__", None) is list:
             data_type = data_type.__args__[0]
-        typename = get_type_hints(data_type)['type'].__args__[0]
+        typename = get_type_hints(data_type)["type"].__args__[0]
         return data_type(
             id=id,
             type=typename,
@@ -61,11 +60,9 @@ class ResponseModel(BaseModel, Generic[DataT]):
             links=links,
         )
 
+
 def JsonApiResponse(
-    type_string: str,
-    attributes_model: Any,
-    *,
-    use_list: bool = False
+    type_string: str, attributes_model: Any, *, use_list: bool = False
 ) -> Type[ResponseModel]:
     response_data_model = ResponseDataModel[
         Literal[type_string],
@@ -73,11 +70,11 @@ def JsonApiResponse(
     ]
     if use_list:
         response_data_model = List[response_data_model]
-        response_data_model.__name__ = f'ListResponseData[{type_string}]'
+        response_data_model.__name__ = f"ListResponseData[{type_string}]"
         response_model = ResponseModel[response_data_model]
-        response_model.__name__ = f'ListResponse[{type_string}]'
+        response_model.__name__ = f"ListResponse[{type_string}]"
     else:
-        response_data_model.__name__ = f'ResponseData[{type_string}]'
+        response_data_model.__name__ = f"ResponseData[{type_string}]"
         response_model = ResponseModel[response_data_model]
-        response_model.__name__ = f'Response[{type_string}]'
+        response_model.__name__ = f"Response[{type_string}]"
     return response_model
